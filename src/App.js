f0e90548f45caedb5adb79fb8d26084fc27cd33b
muhammad-mughees <67280585+muhammad-mughees@users.noreply.github.com>
2020-07-20 02:02:58 +0300

    login bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
+import Navbar from './Navbar'
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, TextField } from '@material-ui/core';
+import { Paper, TextField, Button, } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: 'lightblue',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
-    },
-  },
   Paper: {
-    height: 500,
     width: 400,
     margin: '0 auto',
+    marginTop: 50,
     textAlign: 'center',
+    padding: 15
+  },
+  input: {
+    width: '95%',
+    marginBottom: 10
   }
 }));
 
@@ -26,19 +24,22 @@ function App() {
 
   return (
     <div className={classes.root}>
+      <Navbar />
       <Paper elevation={3} className={classes.Paper}>
         <h1>Login</h1>
 
         <form className={classes.root} noValidate autoComplete="off">
-          <TextField label="Enter Email" />
-          <br />
-          <TextField label="Enter Password" />
+          <TextField label="Enter Email" className={classes.input} />
+          <TextField label="Enter Password" className={classes.input} />
+          <Button variant="contained" color="primary"className={classes.input}>
+            Login
+          </Button>
         </form>
 
 
 
       </Paper>
-    </div>
+    </div >
 
   );
 }
